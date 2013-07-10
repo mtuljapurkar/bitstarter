22d4013d75ed7eb1697f739437be6cde8395bbd2
Mandar Tuljapurkar <mannieeis@gmail.com>
2013-07-10 20:36:01 +0000

    Reading text from index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = fs.readFileSync(index.html);
+var buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
